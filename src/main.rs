11f32e4592355aa5a9202029ce818774f12bdfff
zmoerema <zoe.moeremans@ulb.be>
2024-12-13 11:20:48 +0100

    [UPDATE] move CLI command configuration to method

--- src/main.rs
+++ src/main.rs
@@ -30,51 +30,54 @@ pub fn read_task_file(file_path: &String) -> Result<TaskSet, Box<dyn Error>> {
     Ok(TaskSet::new(tasks))
 }
 
+pub fn build_cli_command() -> Command {
+    Command::new("EDF Scheduler")
+    .version("1.0")
+    .author("Your Name <your_email@example.com>")
+    .about("Simulates EDF scheduling for task sets")
+    .disable_version_flag(true)
+    .disable_help_flag(true)
+    
+    .arg(Arg::new("task_file")
+    .required(true)
+    .help("Path to the task set file"))
+
+    .arg(Arg::new("m")
+        .required(true)
+        .help("Number of cores"))
+
+    .arg(Arg::new("version")
+        .short('v')
+        .long("version")
+        .required(true)
+        .help("Version of EDF to use (global, partitioned, or EDF(k))")
+        .value_parser(["global", "partitioned", "k"]))
+        
+    .arg(Arg::new("workers")
+        .short('w')
+        .long("workers")
+        .help("Number of workers to run the simulation")
+        .default_value("4"))
+
+    .arg(Arg::new("heuristic")
+        .short('h')
+        .long("heuristic")
+        .help("Heuristic to use for partitioned scheduling")
+        .value_parser(["ff", "nf", "bf", "wf"]))
+
+    .arg(Arg::new("sorting")
+        .short('s')
+        .long("sorting")
+        .help("Task ordering based on utilization")
+        .value_parser(["iu", "du"]))
+}
+
 fn main() {
     // cargo run <task_file> <m> -v global|partitioned|<k> [-w <w>] [-h ff|nf|bf|wf] [-s iu|du]
     // example : cargo run taskset.txt 4 -v partitioned -w 8 -h ff -s iu
-    let matches = Command::new("EDF Scheduler")
-        .version("1.0")
-        .author("Your Name <your_email@example.com>")
-        .about("Simulates EDF scheduling for task sets")
-        .disable_version_flag(true)
-        .disable_help_flag(true)
-        
-        .arg(Arg::new("task_file")
-        .required(true)
-        .help("Path to the task set file"))
-
-        .arg(Arg::new("m")
-            .required(true)
-            .help("Number of cores"))
-
-        .arg(Arg::new("version")
-            .short('v')
-            .long("version")
-            .required(true)
-            .help("Version of EDF to use (global, partitioned, or EDF(k))")
-            .value_parser(["global", "partitioned", "k"]))
-            
-        .arg(Arg::new("workers")
-            .short('w')
-            .long("workers")
-            .help("Number of workers to run the simulation")
-            .default_value("4"))
-
-        .arg(Arg::new("heuristic")
-            .short('h')
-            .long("heuristic")
-            .help("Heuristic to use for partitioned scheduling")
-            .value_parser(["ff", "nf", "bf", "wf"]))
-
-        .arg(Arg::new("sorting")
-            .short('s')
-            .long("sorting")
-            .help("Task ordering based on utilization")
-            .value_parser(["iu", "du"]))
-
-        .get_matches();
     
+    let matches = build_cli_command().get_matches();
+
     // Read tasks from file
     let taskset = match read_task_file(matches.get_one::<String>("task_file").unwrap()) {
         Ok(taskset) => taskset,
